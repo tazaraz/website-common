8cff02c312268ebf06074a8db09d96839dc6e889
Emo Maat <emosbm@gmail.com>
2023-01-03 17:01:01 +0100

    Style fixes

--- components/input.vue
+++ components/input.vue
@@ -1,6 +1,6 @@
 <template>
-    <label class="common-input">
-        <input :id="id"
+    <label :id="id" class="common-input">
+        <input
                ref="input"
                :autofocus="focus"
                :type="type"
@@ -51,12 +51,15 @@ export default class Input extends Vue {
     display: inline-block;
     position: relative;
     background: $background-medium;
-    border: 0.2rem solid $background-dark;
     border-radius: $radius-small;
+
+    :focus {
+        outline: none;
+    }
 
     input{
         width: 100%;
-        padding: 1em;
+        padding: 0.6em;
         color: $text;
         border-radius: $radius-small;
     }
@@ -94,10 +97,10 @@ export default class Input extends Vue {
 
 /* Override default */
 input[type="search"]{
-    background-image: url(/img/common/search-small.svg);
+    background-image: url("data:image/svg+xml,%3Csvg fill='%23000000' width='50px' height='50px' viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M602.829 712.369l230.358 233.067c22.423 22.687 58.944 22.853 81.572.373s22.795-59.096.372-81.783l-222.148-224.76c53.94-61.34 86.688-142.107 86.688-230.556 0-191.674-153.786-347.268-343.755-347.268S92.161 217.037 92.161 408.71c0 191.674 153.786 347.268 343.755 347.268 60.591 0 117.501-15.829 166.914-43.608zm-166.914-72.052c-126.02 0-228.394-103.577-228.394-231.608s102.374-231.608 228.394-231.608c126.02 0 228.394 103.577 228.394 231.608S561.935 640.317 435.915 640.317z'/%3E%3C/svg%3E");
     background-repeat: no-repeat;
     background-position: 0.6em center;
-    background-size: 1.4em;
+    background-size: 1.4rem;
     padding-left: 2.5em !important;
 }
 </style>
